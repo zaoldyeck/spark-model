40af82991e96550b4446e394a09aedcaad7a65da
zaoldyeck <zaoldyeck9970@gmail.com>
2015-12-28 11:41:22 +0800

    Change scala version to 2.11

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
+#!/usr/bin/env bash
 
 git pull
 sbt clean package
-spark-submit --class Main target/scala-2.10/spark_2.10-1.0.jar
-#spark-submit --class Main target/scala-2.11/spark_2.11-1.0.jar
+spark-submit --class Main target/scala-2.11/spark_2.11-1.0.jar
