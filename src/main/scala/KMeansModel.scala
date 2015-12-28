19c19da0b9f465f24341bc6795b8bdace1250b74
zaoldyeck <zaoldyeck9970@gmail.com>
2015-12-28 17:44:39 +0800

    Change logger

--- src/main/scala/KMeansModel.scala
+++ src/main/scala/KMeansModel.scala
@@ -1,7 +1,6 @@
-import akka.event.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.apache.spark.SparkContext
 import org.apache.spark.mllib.clustering.KMeans
-import org.apache.spark.mllib.linalg
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.rdd.RDD
 import sys.process._
@@ -12,12 +11,14 @@ import sys.process._
 class KMeansModel {
   val INPUT_PATH = "hdfs://pubgame/user/vincent/efunfun_android_prod_game_for_kmeans.csv"
   val OUTPUT_PATH = "hdfs://pubgame/user/vincent/kmeans"
-  val akkaLogger = Logger("！！This Is Important Message！！")
+  val sparkLogger = LoggerFactory.getLogger(getClass)
 
   def run(sc: SparkContext): Unit = {
     val data: RDD[String] = sc.textFile(INPUT_PATH)
 
     val parsedData = data.map(s => Vectors.dense(s.split(',').map(_.toDouble))).cache()
+
+    sparkLogger.debug("Print Input Data")
     parsedData.foreach(println)
 
     val parsedDataExceptId = parsedData.map(vector => Vectors.dense(vector.toArray.drop(1)))
@@ -29,13 +30,14 @@ class KMeansModel {
 
     // Evaluate clustering by computing Within Set Sum of Squared Errors
     val WSSSE = clusters.computeCost(parsedDataExceptId)
-    akkaLogger.warn("Within Set Sum of Squared Errors = " + WSSSE)
+    sparkLogger.warn("Within Set Sum of Squared Errors = " + WSSSE)
 
     val s = "hadoop fs -rm -f -r " + OUTPUT_PATH
     s.!
 
+    sparkLogger.debug("Print Output Data")
     parsedData zip parsedDataExceptId map {
-      case (vectorWithId, vectorExceptId) => Vectors.dense(vectorExceptId.toArray :+ clusters.predict(vectorExceptId).toDouble)
+      case (vectorWithId, vectorExceptId) => Vectors.dense(vectorWithId.toArray :+ clusters.predict(vectorExceptId).toDouble)
     } foreach println
 
     // Save and load model
