06e8288d58d8c436dd5ae423bf63d03a013cdf42
zaoldyeck <zaoldyeck9970@gmail.com>
2016-01-07 01:52:05 +0800

    Debug

--- src/main/scala/ALSModel3.scala
+++ src/main/scala/ALSModel3.scala
@@ -51,17 +51,6 @@ class ALSModel3 extends ALSModel {
             sc.parallelize(half2Split._2))
       }
 
-      val output_1: String = evaluateModel(trainingData union split._2 union split._3 union split._4, split._1)
-      val output_2: String = evaluateModel(trainingData union split._1 union split._3 union split._4, split._2)
-      val output_3: String = evaluateModel(trainingData union split._1 union split._2 union split._4, split._3)
-      val output_4: String = evaluateModel(trainingData union split._1 union split._2 union split._3, split._4)
-      val printWriter: PrintWriter = new PrintWriter(fileSystem.create(new Path(s"$OUTPUT_PATH/${System.nanoTime}")))
-      Try {
-        printWriter.write(s"$output_1\n$output_2\n$output_3\n$output_4\n-------------------------------\n")
-      } match {
-        case _ => printWriter.close()
-      }
-
       def evaluateModel(trainingData: RDD[Rating], testingData: RDD[Rating]): String = {
         val rank: Int = 10
         val numIterations: Int = 10
@@ -75,6 +64,17 @@ class ALSModel3 extends ALSModel {
           case ((user, product), (predict, fact)) => PredictResult(user, product, predict, fact)
         }
         s"rank:$rank,lambda:$lambda,alpha:$alpha,${calConfusionMatrix(predictResult).toListString}"
+      }
+
+      val output_1: String = evaluateModel(trainingData union split._2 union split._3 union split._4, split._1)
+      val output_2: String = evaluateModel(trainingData union split._1 union split._3 union split._4, split._2)
+      val output_3: String = evaluateModel(trainingData union split._1 union split._2 union split._4, split._3)
+      val output_4: String = evaluateModel(trainingData union split._1 union split._2 union split._3, split._4)
+      val printWriter: PrintWriter = new PrintWriter(fileSystem.create(new Path(s"$OUTPUT_PATH/${System.nanoTime}")))
+      Try {
+        printWriter.write(s"$output_1\n$output_2\n$output_3\n$output_4\n-------------------------------\n")
+      } match {
+        case _ => printWriter.close()
       }
     })), Duration.Inf)
   }
