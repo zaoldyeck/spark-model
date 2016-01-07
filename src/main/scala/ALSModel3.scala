94f300489da4a35ae384ed5086cc00bea710bb7f
zaoldyeck <zaoldyeck9970@gmail.com>
2016-01-07 11:47:11 +0800

    Change split data api

--- src/main/scala/ALSModel3.scala
+++ src/main/scala/ALSModel3.scala
@@ -25,9 +25,8 @@ class ALSModel3 extends ALSModel {
 
   override def run(implicit sc: SparkContext): Unit = {
     val trainingData: RDD[Rating] = mappingData(sc.textFile(TRAINING_DATA_PATH)).persist
-    val predictionData: Array[Rating] = mappingData(sc.textFile(PREDICTION_DATA_PATH)).persist.collect
+    val predictionData: RDD[Rating] = mappingData(sc.textFile(PREDICTION_DATA_PATH)).persist
     val fileSystem: FileSystem = FileSystem.get(new Configuration)
-    val length: Int = predictionData.length
     val delete_out_path: String = "hadoop fs -rm -f -r " + OUTPUT_PATH
     delete_out_path.!
 
@@ -40,16 +39,8 @@ class ALSModel3 extends ALSModel {
 
     Random.shuffle(parametersSeq).foreach(parameters => Await.result({
       case class Prediction(_1: RDD[Rating], _2: RDD[Rating], _3: RDD[Rating], _4: RDD[Rating])
-      val split: Prediction = Random.shuffle(predictionData.toSeq).splitAt(length / 2 - 1) match {
-        case (half1, half2) =>
-          val half1Split: (Seq[Rating], Seq[Rating]) = half1.splitAt(half1.length / 2 - 1)
-          val half2Split: (Seq[Rating], Seq[Rating]) = half2.splitAt(half2.length / 2 - 1)
-          Logger.log.warn("Split Ok")
-          Prediction(
-            sc.parallelize(half1Split._1),
-            sc.parallelize(half1Split._2),
-            sc.parallelize(half2Split._1),
-            sc.parallelize(half2Split._2))
+      val split: Prediction = predictionData.randomSplit(Array(0.25, 0.25, 0.25, 0.25)) match {
+        case Array(split_1, split_2, split_3, split_4) => Prediction(split_1, split_2, split_3, split_4)
       }
 
       case class Evaluation(output: String, recall: Double) {
