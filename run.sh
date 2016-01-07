36c45d4a112565b62a5d8ddfbfde4ec21d58b9d6
zaoldyeck <zaoldyeck9970@gmail.com>
2016-01-07 13:36:12 +0800

    Cancel long run

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 git pull
 sbt clean package
-spark-submit —queue longrun --class Main target/scala-2.11/spark-model_2.11-1.0.jar
+spark-submit --class Main target/scala-2.11/spark-model_2.11-1.0.jar
