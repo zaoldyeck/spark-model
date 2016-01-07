6ef90ba66039ab6fffcabe76a48ffc499663b9b4
zaoldyeck <zaoldyeck9970@gmail.com>
2016-01-07 13:31:31 +0800

    Test

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 git pull
 sbt clean package
-spark-submit --class Main target/scala-2.11/spark-model_2.11-1.0.jar —-queue longrun
+spark-submit —queue longrun --class Main target/scala-2.11/spark-model_2.11-1.0.jar
