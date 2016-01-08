79e41c19f11a69072f98397484dfdeb122fe3998
zaoldyeck <zaoldyeck9970@gmail.com>
2016-01-08 18:49:05 +0800

    Test

--- src/main/scala/FormatData.scala
+++ src/main/scala/FormatData.scala
@@ -12,8 +12,8 @@ class FormatData(implicit sc: SparkContext) {
     import sql.implicits._
     new HiveContext(sc).table("pg_with_gd_target_data").select(
       $"unique_id".cast("Int"),
-      $"game_id".cast("Int"),
-      $"saving".cast("Double").when($"saving" > 0, 1).otherwise(2).as("saving"))
+      $"game_id".cast("Int"))
+      //$"saving".cast("Double").when($"saving" > 0, 1).otherwise(2).as("saving"))
       .filter($"max_login_days".cast("Int") >= 1 and $"max_login_times".cast("Int") >= 2)
       .write.parquet("user_game")
   }
