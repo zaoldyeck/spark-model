fd5c481b53e3b202c84fad99f004aa56ce0902e9
zaoldyeck <zaoldyeck9970@gmail.com>
2016-01-12 11:29:38 +0800

    Test

--- src/main/scala/Logger.scala
+++ src/main/scala/Logger.scala
@@ -4,6 +4,6 @@ import org.apache.log4j.{Level, LogManager}
   * Created by zaoldyeck on 2015/12/29.
   */
 object Logger extends Serializable {
-  @transient lazy val log = LogManager.getRootLogger
+  val log = LogManager.getRootLogger
   log.setLevel(Level.WARN)
 }
