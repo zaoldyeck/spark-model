cf122d42845c153e17db97f4e3f0ec1441e543d2
zaoldyeck <zaoldyeck9970@gmail.com>
2016-01-13 04:03:28 +0800

    Debug

--- src/main/scala/ALSModel3.scala
+++ src/main/scala/ALSModel3.scala
@@ -19,7 +19,7 @@ import scala.util.Random
 /**
   * Created by zaoldyeck on 2016/1/6.
   */
-class ALSModel3 {
+class ALSModel3 extends Serializable {
 
   case class DataSet(trainingData: RDD[Rating], predictionData: RDD[Rating], outputPath: String)
 
