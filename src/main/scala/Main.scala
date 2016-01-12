02eb318485d9acca713fcabfae51ee4157317db9
zaoldyeck <zaoldyeck9970@gmail.com>
2016-01-12 18:32:31 +0800

    Test

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,7 +12,8 @@ object Main {
     sc.setCheckpointDir("checkpoint")
     //new ALSModel().run
     //new ALSModel2().run
-    new ALSModel3().run()
+    //new ALSModel3().run()
+    new ALSModel4().run()
     //new KMeansModel().run
     //new LDAModel().run
     //new TestALSModel().run
