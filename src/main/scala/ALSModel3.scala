55c57abec73bc6bc9d38261c403a53f95e6d207d
zaoldyeck <zaoldyeck9970@gmail.com>
2016-01-12 20:11:04 +0800

    Debug

--- src/main/scala/ALSModel3.scala
+++ src/main/scala/ALSModel3.scala
@@ -19,7 +19,7 @@ import scala.util.Random
 /**
   * Created by zaoldyeck on 2016/1/6.
   */
-class ALSModel3(implicit sc: SparkContext) extends ALSModel {
+class ALSModel3(implicit sc: SparkContext) extends ALSModel with Serializable {
   /*
   private val sqlContext: SQLContext = new SQLContext(sc)
   */
